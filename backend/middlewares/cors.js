e0440daeecc85c5cfcd4903eeebfcaef0feaa034
Karolina Lipnitskaite <lipnitskaite@gmail.com>
2022-07-16 18:23:32 +0300

    fix: cors

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -14,12 +14,12 @@ exports.checkCors = async (req, res, next) => {
 
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', 'true');
   }
 
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
-    res.header('Access-Control-Allow-Credentials', 'true');
 
     return res.end();
   }
