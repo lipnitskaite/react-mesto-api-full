2832b9ea74850bb91bd3330f50a868d5c9222839
Karolina Lipnitskaite <lipnitskaite@gmail.com>
2023-10-12 15:47:48 +0200

    #5-install-sass-and-rewrite-App-component-using-scss-modules

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from '../src/components/App';
+import App from '../src/components/App/App';
 import reportWebVitals from './reportWebVitals';
 import { HashRouter } from 'react-router-dom';
 
