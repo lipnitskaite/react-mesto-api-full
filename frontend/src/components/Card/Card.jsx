5bdfe22b5c41eb2471b0b14ed4ce18212f45a659
Karolina Lipnitskaite <lipnitskaite@gmail.com>
2023-10-17 14:26:08 +0200

    #5-Card-add-scss-modules

---
+++ frontend/src/components/Card/Card.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { CurrentUserContext } from "../../contexts/CurrentUserContext";
+import styles from "./Card.module.scss";
+
+function Card({ card, onCardClick, onCardLike, onCardDelete }) {
+  const currentUser = React.useContext(CurrentUserContext);
+  const isOwn = card.owner === currentUser.id;
+  const cardDeleteButtonClassName = `${styles.delete_button} ${
+    !isOwn && styles.delete_button_inactive
+  }`;
+
+  const isLiked = card.likes.some((like) => like === currentUser.id);
+  const cardLikeButtonClassName = `${styles.like_button} ${
+    isLiked ? styles.like_button_active : styles.like_button_inactive
+  }`;
+
+  const handleLikeClick = () => onCardLike(card);
+  const handleDeleteClick = () => onCardDelete(card);
+
+  const handleCardClick = () => onCardClick(card);
+  return (
+    <article className={styles.card}>
+      <img
+        src={card.link}
+        alt={card.name}
+        className={styles.image}
+        onClick={handleCardClick}
+      />
+      <div className={styles.main}>
+        <h2 className={styles.title}>{card.name}</h2>
+        <div className={styles.like_container}>
+          <button
+            className={cardLikeButtonClassName}
+            type='button'
+            onClick={handleLikeClick}
+          ></button>
+          <p className={styles.like_number}>{card.likes.length}</p>
+        </div>
+        <button
+          className={cardDeleteButtonClassName}
+          type='button'
+          onClick={handleDeleteClick}
+        ></button>
+      </div>
+    </article>
+  );
+}
+
+export default Card;
--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import Card from './Card';
-import { CurrentUserContext } from '../contexts/CurrentUserContext';
+import React from "react";
+import Card from "./Card/Card";
+import { CurrentUserContext } from "../contexts/CurrentUserContext";
 
 function Main({
   onEditAvatar,
@@ -13,33 +13,49 @@ function Main({
 }) {
   const currentUser = React.useContext(CurrentUserContext);
 
-    return (
-      <main className="content">
-        <section className="profile">
-          <div className="profile__photo-container">
-            <img src={currentUser.avatar} className="profile__photo" alt="Аватар пользователя"/>
-            <button className="profile__photo-edit-button" type="button" onClick={onEditAvatar}></button>
-          </div>
-          <div className="profile__main">
-            <h1 className="profile__title">{currentUser.name}</h1>
-            <button className="profile__edit-button" type="button" onClick={onEditProfile}></button>
-          </div>
-          <p className="profile__subtitle">{currentUser.about}</p>
-          <button className="profile__add-button" type="button" onClick={onAddCard}></button>
-        </section>
+  return (
+    <main className='content'>
+      <section className='profile'>
+        <div className='profile__photo-container'>
+          <img
+            src={currentUser.avatar}
+            className='profile__photo'
+            alt='Аватар пользователя'
+          />
+          <button
+            className='profile__photo-edit-button'
+            type='button'
+            onClick={onEditAvatar}
+          ></button>
+        </div>
+        <div className='profile__main'>
+          <h1 className='profile__title'>{currentUser.name}</h1>
+          <button
+            className='profile__edit-button'
+            type='button'
+            onClick={onEditProfile}
+          ></button>
+        </div>
+        <p className='profile__subtitle'>{currentUser.about}</p>
+        <button
+          className='profile__add-button'
+          type='button'
+          onClick={onAddCard}
+        ></button>
+      </section>
 
-        <section className="cards page__cards">
-          {cards.map((card) => (
-            <Card
-              card={card} 
-              onCardClick={onCardClick}
-              onCardLike={onCardLike}
-              onCardDelete={onCardDelete}
-            />
-          )) }
-        </section>
-      </main>
-    );
+      <section className='cards page__cards'>
+        {cards.map((card) => (
+          <Card
+            card={card}
+            onCardClick={onCardClick}
+            onCardLike={onCardLike}
+            onCardDelete={onCardDelete}
+          />
+        ))}
+      </section>
+    </main>
+  );
 }
 
 export default Main;
