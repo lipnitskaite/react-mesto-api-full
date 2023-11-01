2c0973ee981e87b969f4916ae212fb6b2954f2b3
Karolina Lipnitskaite <lipnitskaite@gmail.com>
2023-11-01 15:56:54 +0100

    Enhancement - #19 - Add-create-card-validation

--- frontend/src/components/Modals/AddCardModal/AddCardModal.jsx
+++ frontend/src/components/Modals/AddCardModal/AddCardModal.jsx
@@ -1,12 +1,24 @@
 import React, { useState, useEffect } from "react";
+import { useForm } from "react-hook-form";
 import ModalWithForm from "../ModalWithForm/ModalWithForm";
 import styles from "./AddCardModal.module.scss";
 
 function AddCardModal({ isOpen, onClose, onAddPlace }) {
-  const handleClose = () => onClose();
-
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
   const [cardName, setCardName] = useState("");
   const [cardPhotoLink, setCardPhotoLink] = useState("");
+
+  const handleClose = () => onClose();
+  const onSubmit = () => {
+    onAddPlace({
+      name: cardName,
+      link: cardPhotoLink,
+    });
+  };
 
   function handleCardNameChange(e) {
     setCardName(e.target.value);
@@ -20,15 +32,6 @@ function AddCardModal({ isOpen, onClose, onAddPlace }) {
     setCardPhotoLink("");
   }, [isOpen]);
 
-  function handleSubmit(e) {
-    e.preventDefault();
-
-    onAddPlace({
-      name: cardName,
-      link: cardPhotoLink,
-    });
-  }
-
   return (
     <ModalWithForm
       name='add-post'
@@ -36,33 +39,67 @@ function AddCardModal({ isOpen, onClose, onAddPlace }) {
       buttonTitle='Create'
       isOpen={isOpen}
       onClose={handleClose}
-      onSubmit={handleSubmit}
+      onSubmit={handleSubmit(onSubmit)}
       children={[
         <fieldset className={styles.container}>
-          <input
-            id='title-input'
-            className={styles.input}
-            placeholder='Name'
-            type='text'
-            name='name'
-            value={cardName || ""}
-            onChange={handleCardNameChange}
-            required
-            minLength='2'
-            maxLength='30'
-          />
-          <span className={styles.error}></span>
-          <input
-            id='image-input'
-            className={styles.input}
-            placeholder='Image link'
-            type='url'
-            name='link'
-            value={cardPhotoLink || ""}
-            onChange={handleCardPhotoLinkChange}
-            required
-          />
-          <span className={styles.error}></span>
+          <div
+            className={`${styles.inputContainer} ${
+              errors?.name && styles.inputContainer_error
+            }`}
+          >
+            <input
+              className={`${styles.input} ${
+                errors?.name && styles.input_error
+              }`}
+              {...register("name", {
+                required: "Please fill out this field.",
+                minLength: {
+                  value: 2,
+                  message: "Input must be minimum 2 characters long.",
+                },
+                maxLength: {
+                  value: 30,
+                  message: "Input must be maximum 30 characters long.",
+                },
+              })}
+              id='title-input'
+              placeholder='Name'
+              type='text'
+              name='name'
+              value={cardName}
+              onChange={handleCardNameChange}
+            />
+            {errors?.name && (
+              <span className={styles.error}>{errors?.name?.message}</span>
+            )}
+          </div>
+          <div
+            className={`${styles.inputContainer} ${
+              errors?.link && styles.inputContainer_error
+            }`}
+          >
+            <input
+              className={`${styles.input} ${
+                errors?.link && styles.input_error
+              }`}
+              {...register("link", {
+                required: "Please fill out this field.",
+                minLength: {
+                  value: 2,
+                  message: "Input must be minimum 2 characters long.",
+                },
+              })}
+              id='image-input'
+              placeholder='Image link'
+              type='url'
+              name='link'
+              value={cardPhotoLink || ""}
+              onChange={handleCardPhotoLinkChange}
+            />
+            {errors?.link && (
+              <span className={styles.error}>{errors?.link?.message}</span>
+            )}
+          </div>
         </fieldset>,
       ]}
     />
